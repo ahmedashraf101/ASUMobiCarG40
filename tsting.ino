976d5642ec8d2f145e51804130954e1788822bfe
kholoud97 <33068887+kholoud97@users.noreply.github.com>
2018-02-20 23:29:18 +0200

    Add files via upload

---
+++ tsting.ino
@@ -0,0 +1,24 @@
+
+
+const int ledPin= 11; 
+int i = 0;         
+void setup(
+)
+{ 
+pinMode(ledPin, OUTPUT); 
+} 
+void loop()
+{ 
+for (i = 0; i < 255; i++)
+{ 
+analogWrite(ledPin, i);      
+delay(10); 
+}
+for (i = 255; i > 0; i
+--
+) 
+{
+analogWrite(ledPin, i); 
+delay(10);       
+}
+}
