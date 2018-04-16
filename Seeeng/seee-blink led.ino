921650c80facd685180aa74e2a4f3e424a11dd11
Samir-Elshenawy <36424950+Samir-Elshenawy@users.noreply.github.com>
2018-04-16 22:55:12 +0300

    Add files via upload

---
+++ Seeeng/seee-blink led.ino
@@ -0,0 +1,14 @@
+const int LED = 11;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED, LOW);
+  delay(300); // Wait for 300 millisecond(s)
+}
