7702406b3325b9c7d946066ab76eed9bbe783a7a
Samir-Elshenawy <36424950+Samir-Elshenawy@users.noreply.github.com>
2018-04-18 01:57:56 +0300

    Add files via upload

---
+++ 2_push_bottons.ino
@@ -0,0 +1,24 @@
+//Example 3 2 push bottons 
+
+const int LED = 13;
+const int ON=2;
+const int OFF=3; 
+ void setup ()
+ {
+  pinMode(LED ,OUTPUT);
+  pinMode(ON , INPUT);
+  pinMode(OFF , INPUT);
+  
+ }
+void loop ()
+{
+  if (digitalRead (ON)==HIGH)
+  {
+  digitalWrite (LED,LOW);
+  }
+  else if (digitalRead (OFF)==HIGH)
+  {
+  digitalWrite (LED,HIGH);
+  }
+}
+
