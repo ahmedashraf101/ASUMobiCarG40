49ec817d16f3744c8c7d828d94f32fb74ac66f02
Hamiedamr <hamedamr95@hotmail.com>
2018-06-17 00:13:34 +0200

    Add files via upload
    demo Mode oop

---
+++ demo.ino
@@ -0,0 +1,40 @@
+class Project{
+  // @string mode
+  private:
+    char* _mode; 
+   public:
+   /**
+    *@param mode string
+    *initially  assign _mode to @param mode
+   */ 
+   
+    Project(char * mode){
+      this->_mode = mode;
+    }
+    /**
+     * setMode prototype
+    * @param mode string
+    */
+    void setMode(char * mode);
+  };
+  void  Project::setMode(char * mode){
+      _mode = mode;
+      Serial.print(*(_mode));
+    }
+
+    //Initialize Project
+Project project("goo");
+void setup(){
+  //Mode setting
+  project.setMode("Mode");
+
+  
+}
+
+void loop(){
+  
+
+  
+}
+
+
